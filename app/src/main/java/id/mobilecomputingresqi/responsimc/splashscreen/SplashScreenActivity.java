8a8d2b131cbf824c2352c4fcef6fee78c65bc54e
RezkyZone <rezkyuchiha42@gmail.com>
2020-08-15 13:10:51 +0700

    Responsi

---
+++ app/src/main/java/id/mobilecomputingresqi/responsimc/splashscreen/SplashScreenActivity.java
@@ -0,0 +1,29 @@
+package id.mobilecomputingresqi.responsimc.splashscreen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import id.mobilecomputingresqi.responsimc.R;
+import id.mobilecomputingresqi.responsimc.login.LoginActivity;
+
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, LoginActivity.class));
+                finish();
+            }
+        }, 2000);
+
+    }
+}
